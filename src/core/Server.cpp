61a705501a22a274a8277c11ec51c6a0e239d109
Kacper <kacper.orzechowski.04@gmail.com>
2026-02-06 15:45:29 +0100

    main server.cpp code refactored. merge with parsing part needed

--- src/core/Server.cpp
+++ src/core/Server.cpp
@@ -1,118 +1,129 @@
-// C++ program to show the example of server application in
-// socket programming
-#include <cstring>
-#include <iostream>
-#include <netinet/in.h>
-#include <stdexcept>
-#include <sys/socket.h>
-#include <sys/epoll.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <vector>
-#include <sstream>
-#include <cerrno>
 #include "Server.hpp"
 
 using namespace std;
 
 
-Server::Server(int port) : port(port) {  }
+//Constructors
+Server::Server(int port) :  port(port) 
+{  
+	this->server_init();
+}
 Server::~Server(void) {  }
 
-int const Server::get_socket(void) {return socket_fd;}
-int const Server::get_epoll_fd(void) {return epoll_fd;}
+
+//Member getters
+int Server::get_port(void) const {return port;}
+int Server::get_socket(void) const {return socket_fd;}
+int Server::get_epoll_fd(void) const {return epoll_fd;}
 struct epoll_event Server::get_server_event(void) {return event;}
 struct epoll_event Server::get_active_event(int index) {return active_events[index];} 
 
+
+//Helper functions
+bool accept_connection(Server &serv)
+{
+	int clientSocket = accept(serv.get_socket(), NULL, NULL);
+	if (clientSocket == -1)
+	{
+		if (errno == EAGAIN || errno == EWOULDBLOCK)
+			return false;
+		return false;
+	}
+	cout << "New Client connected." << endl;
+	int status = fcntl(clientSocket, F_SETFL, O_NONBLOCK);
+	if (status == -1)
+		cerr << "fcntl error\n";
+	struct epoll_event event = serv.get_server_event();
+	event.events = EPOLLIN | EPOLLET;
+	event.data.fd = clientSocket;
+	epoll_ctl(serv.get_epoll_fd(), EPOLL_CTL_ADD, clientSocket, &event);
+	return true;
+}
+
+string process_request(string &request_str)
+{
+	//pass request to request parser
+	(void)request_str; // Silences the "unused parameter" error
+    return "HTTP/1.1 200 OK\r\n\r\nHello World";
+}
+
+void respond_to_client(struct epoll_event client, string &response_str)
+{
+	/*2. Before write I should change flag of the socket
+	 *and then write to the client socket*/
+
+	write(client.data.fd, response_str.c_str(), response_str.length());
+	
+	close(client.data.fd);
+}
+
+
+//Memeber functions
 void Server::server_init(void)
 {
-	int server_epoll_fd = epoll_create(10);
-	this->epoll_fd = server_epoll_fd;
+	this->epoll_fd = epoll_create(10);
 
 	this->event.events = EPOLLIN | EPOLLOUT | EPOLLET;
 
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(this->get_port());
+    serverAddress.sin_port = htons(port);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    int serv_socket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-	this->socket_fd = serv_socket;
+    this->socket_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 
 	int opt = 1;
-	if (setsockopt(serv_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+	if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
 	{
-		close(serv_socket);
-		throw std::runtime_error("setsockopt error(SO_REUSEADDR)");
+		close(socket_fd);
+		throw runtime_error("setsockopt error(SO_REUSEADDR)");
 	}
 
-	this->event.data.fd = serv_socket;
+	this->event.data.fd = socket_fd;
 
-    if (bind(serv_socket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
+    if (bind(socket_fd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1)
 	{
-		close(serv_socket);
-		close(server_epoll_fd);
-		throw std::runtime_error("binding error");
+		close(socket_fd);
+		close(epoll_fd);
+		throw runtime_error("binding error");
 	}
 
-	listen(serv_socket, 5);
+	listen(socket_fd, 5);
 
-	if (epoll_ctl(server_epoll_fd, EPOLL_CTL_ADD, serv_socket, &event))
+	if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &event))
 	{
-		std::stringstream ss;
-		ss << errno;
-		throw std::runtime_error(ss.str() + ": epoll_ctl: ADD");
+		throw runtime_error("epoll_ctl ADD");
 	}
 }
 
-int main()
+void Server::server_loop(void)
 {
-
-    // specifying the address
-    // creating socket
-
-
-    // binding socket.
-	// listening to the assigned socket
-	
 	while (true)
 	{
-		int num_fds = epoll_wait(epoll_fd, events, 10, -1);
+		int num_fds = epoll_wait(epoll_fd, active_events, 10, -1);
 		for (int i = 0; i < num_fds; i++)
 		{
-			if (events[i].data.fd == serverSocket)
+			if (active_events[i].data.fd == socket_fd)
 			{
 				while(true)
-				{
-					// accepting connection request
-					int clientSocket
-						= accept(serverSocket, NULL, NULL);
-					if (clientSocket == -1)
-					{
-						if (errno == EAGAIN || errno == EWOULDBLOCK)
-							break;
+					if (!accept_connection(*this))
 						break;
-					}
-					std::cout << "New Client connected." << std::endl;
-					//set_nonblocking(clientSocket);
-					int status = fcntl(clientSocket, F_SETFL, fcntl(clientSocket, F_GETFL, 0) | O_NONBLOCK);
-					if (status == -1)
-						std::cerr << "fcntl error\n";
-					event.events = EPOLLIN | EPOLLET;
-					event.data.fd = clientSocket;
-					epoll_ctl(epoll_fd, EPOLL_CTL_ADD, clientSocket, &event);
-				}
 			}
 			else
 			{
 				char buffer[120] = { 0 };
-				std::stringstream entire_request;
+				stringstream entire_request;
 				while (true)
 				{
-					ssize_t count = read(events[i].data.fd, buffer, sizeof(buffer));
+					ssize_t count = read(active_events[i].data.fd, buffer, sizeof(buffer));
 					if (count == -1)
 					{
+						string request_content = entire_request.str();
+						string response_str = process_request(request_content);
+						if (!response_str.empty())
+							respond_to_client(active_events[i], response_str);
+						else
+							throw runtime_error("empty response");
 						/* 1. call REQUEST_PARSER with entire_request.str() as parameter
 						 *it should return string to respond to the client.*/
 
@@ -126,8 +137,8 @@ int main()
 					}
 					else if (count == 0)
 					{
-						std::cout << "Client: " << i << " disconnected." << std::endl;
-						close(events[i].data.fd);
+						cout << "Client: " << i << " disconnected." << endl;
+						close(active_events[i].data.fd);
 						break;
 					}
 					else
@@ -141,7 +152,21 @@ int main()
 
     // closing the socket.
 	close(epoll_fd);
-    close(serverSocket);
+    close(socket_fd);
+}
 
-    return 0;
+int main()
+{
+	Server server(8080);
+
+	try
+	{
+		server.server_init();
+		server.server_loop();
+	}
+	catch (runtime_error &e)
+	{
+		cout << RED << "ERROR: " << e.what() << RESET << endl;
+	}
+	return 0;
 }
--- src/core/Server.hpp
+++ src/core/Server.hpp
@@ -2,6 +2,20 @@
 #define SERVER_HPP
 
 #include <sys/epoll.h>
+#include <iostream>
+#include <stdexcept>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cerrno>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sstream>
+
+
+static const std::string RED = "\033[0;31m";
+static const std::string RESET = "\033[0m";
+static const std::string GREEN = "\033[0;32m";
+
 class Server
 {
 	private:
@@ -12,11 +26,12 @@ class Server
 	public:
 		Server(int port);
 		~Server(void);
-		int const get_port(void);
-		int const get_socket(void);
-		int const get_epoll_fd(void);
+		int get_port(void) const;
+		int get_socket(void) const;
+		int get_epoll_fd(void) const;
 		struct epoll_event get_server_event(void);
 		struct epoll_event get_active_event(int index);
 		void server_init(void);
+		void server_loop(void);
 };
 #endif
