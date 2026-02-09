e9fda403ff821bb03185e372749977fdd211ac8d
kinga <kinga.kwasniak5@gmail.com>
2026-02-09 16:53:26 +0100

    staring making a request

--- include/http/Request.hpp
+++ include/http/Request.hpp
@@ -13,12 +13,44 @@
 #ifndef REQUEST_HPP
 #define REQUEST_HPP
 
-#include <iostream>
-#include <list>
-#include <map>
-#include <sstream>
-#include <string>
-#include <sys/stat.h>
+# include <iostream>
+# include <list>
+# include <map>
+# include <sstream>
+# include <string>
+# include <sys/stat.h>
+# include "Parser.hpp"
+# include <vector>
+# include <cstdlib>
 
+using namespace std;
+
+class Request
+{
+	private:
+			string raw_request;
+			map<string, string> headers;
+			string method, path, version;
+			string body;
+			bool is_valid;
+			int error_code;
+	public:
+			//constructors
+			Request();
+			Request(const string &request);
+			Request(const Request &other);
+			~Request();
+			//getters
+			string get_Method() const;
+			string get_Path() const;
+			string get_Version() const;
+			string get_Body() const;
+			map<string, string> get_Headers() const;
+			string get_Raw_Request() const;
+			bool is_Valid() const;
+			int get_Error_Code() const;
+
+
+}
 
 #endif
--- src/http/Request.cpp
+++ src/http/Request.cpp
@@ -11,3 +11,57 @@
 /* ************************************************************************** */
 
 #include "http/Request.hpp"
+
+//constructors
+
+Request::Request()
+{
+	raw_request = "";
+	method = "";
+	path = "";
+	version = "";
+	body = "";
+	is_valid = false;
+	error_code = 0;
+}
+
+Request::Request(const string &request) 
+{
+	raw_request = request;
+	method = "";
+	path = "";
+	version = "";
+	body = "";
+	headers.clear();
+	is_valid = false;
+	error_code = 0;
+}
+
+Request::Request(const Request &other)
+{
+	if(this != &other)
+	{
+		raw_request = other.raw_request;
+		method = other.method;
+		path = other.path;
+		version = other.version;
+		body = other.body;
+		headers = other.headers;
+		is_valid = other.is_valid;
+		error_code = other.error_code;
+	}
+}
+
+Request::~Request() {};
+
+//getters
+
+string Request::get_Method() const {return method;}
+string Request::get_Path() const { return path; }
+string Request::get_Version() const { return version; }
+string Request::get_Body() const { return body; }
+map<string, string> Request::get_Headers() const { return headers; }
+string Request::get_Raw_Request() const { return raw_request; }
+bool Request::is_Valid() const { return is_valid; }
+int Request::get_Error_Code() const { return error_code; }
+
