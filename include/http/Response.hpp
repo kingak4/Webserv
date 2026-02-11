16d6cc739e5d52031e29e0aa46a1f9a253ec4342
kinga <kinga.kwasniak5@gmail.com>
2026-02-11 12:38:33 +0100

    starting crateing response makieng new clas planning work and functions

--- include/http/Response.hpp
+++ include/http/Response.hpp
@@ -16,11 +16,40 @@
 # include <dirent.h>
 # include <iostream>
 # include <string>
+# include <vector>
+# include <cstdlib>
+# include <list>
+# include <map>
+
+using namespace std;
 
 class Response
 {
-	
-}
+	private:
+		int status_code;
+		string status_text;
+		map<string, string> headers;
+		string body;
+		string raw_response;
+	public:
+		//constructors
+		Response();
+		Response(const Response &other);
+		~Response();
+		//getters
+		int get_Status_Code() const;
+		string get_Status_Text() const;
+		map<string, string> get_Headers() const;
+		map<string, string> get_Headers() const;
+		string get_Body() const;
+		string get_Raw_Response() const;
+		bool is_Valid() const;
+		int get_Status_Code() const;
+		string get_Status_text() const;
+		//functions
+		void reset();
+
+};
 
 
 #endif
--- src/http/Response.cpp
+++ src/http/Response.cpp
@@ -11,3 +11,44 @@
 /* ************************************************************************** */
 
 #include "http/Response.hpp"
+
+Response::Response()
+{
+	status_code = 0;
+	status_text = "";
+	body = "";
+	raw_response = "";
+}
+
+Response::Response(const Response &other)
+{
+	if(this != &other)
+	{
+		status_code = other.status_code;
+		status_text = other.status_text;
+		headers = other.headers;
+		body = other.body;
+		raw_response = other.raw_response;
+	}
+}
+
+Response::~Response() {};
+
+//getters
+
+int Response::get_Status_Code() const {return status_code;}
+string Response::get_Status_Text() const {return status_text;}
+string Response::get_Body() const {return body;}
+map<string, string> Response::get_Headers() const {return headers;}
+std::string Response::get_Raw_Response() const {return raw_response;}
+
+//functions
+
+void Response::reset()
+{
+	status_code = 0;
+	status_text = "";
+	headers.clear();
+	body = "";
+	raw_response = "";
+}
