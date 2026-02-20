05d3cc486438fde283990bd79b184e45d4275160
Alina Ralcewicz <alina.raltsevich@gmail.com>
2026-02-20 15:16:04 +0100

    Merge main to the branch. Remove comments

--- www/cgi-bin/test.py
+++ www/cgi-bin/test.py
@@ -4,28 +4,20 @@ import os
 import sys
 from urllib.parse import parse_qs
 
-# Determine request method
 method = os.environ.get("REQUEST_METHOD", "GET")
-# Initialize variable
 user_name = "Guest"
 
 if method.upper() == "POST":
-    # Read POST data from stdin
-    content_length_str = os.environ.get("CONTENT_LENGTH")
     content_length = int(os.environ.get("CONTENT_LENGTH") or 0)
     post_data = sys.stdin.read(content_length)
 
-    # Parse the data
-    # post_data = "name=Alice"
     form = parse_qs(post_data)
     user_name = form.get("name", ["Guest"])[0]
     print('USER NAME', user_name)
 
-# Send HTTP header
 print("Content-Type: text/html")
-print()  # Blank line separates header from body
+print()
 
-# Output HTML
 print(f"""<html>
 <head><title>CGI POST Demo</title></head>
 <body>
